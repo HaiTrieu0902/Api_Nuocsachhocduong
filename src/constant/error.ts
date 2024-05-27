efd40a0b1fa53e2c6405db5e4181c33cf5311398
Trieu Bui Hai <haitrieuql0902@gmail.com>
2024-05-27 13:43:23 +0700

    update config

---
+++ src/constant/error.ts
@@ -0,0 +1,20 @@
+export const MESSAGES_ERROR = {
+  ACCOUNT_NOT_EXIST: 'This account does not exist in the system',
+  WRONG_EMAIL_OR_PASSWORD: 'Username or password is incorrect',
+  USER_NOT_FOUND: 'User not found',
+  TOKEN_INVALID: 'Token invalid, please contact our hotline for support',
+  EMAIL_EXIST: 'Email already exists',
+  EMAIL_INVALID: 'Please use email is invited by admin',
+  OLD_PASSWORD_INVALID: 'Old password is incorrect',
+  COMMON_ERROR: 'Error, please try again.',
+  EMAIL_IS_NOT_EXIST: 'Email is not exists.',
+  REQUEST_IS_EXPIRED: 'Request is expired.',
+  CAN_NOT_DELETE_ACTIVE_USER: 'Can not delete active user',
+  USER_INACTIVE: 'Your account doesn’t exist in system, please contact our hotline for more details',
+  DRIVER_CONNECTION_INVALID: 'Driver connection already exists',
+  EMAIL_USER_EXIST: 'already exists',
+};
+
+export const USER_ERROR = {
+  EMAIL_EXIST: 'Email already exists',
+};
